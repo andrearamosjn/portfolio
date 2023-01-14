5c80c9a0016164648cc4fb8d18333128f1c0a338
Xavi Moreno <xavier.moreno.martinez@gmail.com>
2023-01-14 21:25:13 +0100

    minor layout updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import { PropsWithChildren } from "react";
-import './globals.css';
+import "./globals.css";
 
 const links = [
   { link: "/me", label: "Me" },
   { link: "/work", label: "Work" },
-]
+];
 
 export default function BaseLayout({ children }: PropsWithChildren) {
   const pathName = usePathname();
@@ -19,10 +19,15 @@ export default function BaseLayout({ children }: PropsWithChildren) {
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="w-screen h-screen bg-[#f6f7f0] pt-20">
-        <div className="flex flex-row bg-[#f6f7f0] w-screen z-10 fixed top-0">
-          <a href="/" className="p-5 pl-20 max-sm:pl-10 font-[Heebo] text-3xl">ARJ</a>
-          <nav className="flex-1 flex pr-20 max-sm:pr-10 max-sm:flex-row-reverse">
-            <a href="#menu" className="space-y-2 p-5 pr-0 sm:hidden max-sm:block">
+        <div className="flex flex-row bg-[#f6f7f0]/30 backdrop-blur-xl w-screen z-10 fixed top-0">
+          <a href="/" className="p-5 pl-20 max-sm:pl-5 font-[Heebo] text-3xl">
+            ARJ
+          </a>
+          <nav className="flex-1 flex pr-20 max-sm:pr-5 max-sm:flex-row-reverse">
+            <a
+              href="#menu"
+              className="space-y-2 p-5 pr-0 sm:hidden max-sm:block"
+            >
               <div className="block h-1 w-8 bg-gray-800" />
               <div className="block h-1 w-8 bg-gray-800" />
               <div className="block h-1 w-8 bg-gray-800" />
@@ -31,13 +36,12 @@ export default function BaseLayout({ children }: PropsWithChildren) {
               {links.map(({ link, label }) => (
                 <li
                   key={link}
-                  className={`p-5 font-[Heebo] text-2xl font-thin uppercase hover:font-light active:font-normal focus:font-normal transition-all ${pathName?.startsWith(link) ? 'font-light' : ''}`}
+                  className={`p-5 font-[Heebo] text-2xl font-thin uppercase hover:font-light active:font-normal focus:font-normal transition-all ${
+                    pathName?.startsWith(link) ? "font-light" : ""
+                  }`}
                 >
-                  <a href={link}>
-                    {label}
-                  </a>
-                </li
->
+                  <a href={link}>{label}</a>
+                </li>
               ))}
             </ul>
           </nav>
@@ -45,22 +49,24 @@ export default function BaseLayout({ children }: PropsWithChildren) {
         {children}
         <div
           id="menu"
-          className="top-0 left-0 bg-accent absolute w-screen h-screen hidden target:flex"
+          className="top-0 left-0 bg-accent absolute w-screen h-screen hidden target:flex z-10"
         >
-          <a href="#" className="text-8xl absolute right-8 font-thin font-[Heebo] top-0 leading-[.5]">&times;</a>
-          <ul className="flex flex-1 flex-col content-center justify-center gap-5">
-              {links.map(({ link, label }) => (
-                <li
-                  key={link}
-                  className="p-5 font-[Heebo] text-5xl font-thin uppercase hover:font-light active:font-normal focus:font-normal transition-all text-center"
-                >
-                  <a href={link}>
-                    {label}
-                  </a>
-                </li
->
-              ))}
-            </ul>
+          <a
+            href="#"
+            className="text-7xl absolute right-5 font-light font-[Heebo] top-3 leading-[.5]"
+          >
+            &times;
+          </a>
+          <ul className="flex flex-1 flex-col content-center flex-col-reverse justify-center gap-5">
+            {links.map(({ link, label }) => (
+              <li
+                key={link}
+                className="p-5 font-[Heebo] text-5xl font-thin uppercase hover:font-light active:font-normal focus:font-normal transition-all text-center"
+              >
+                <a href={link}>{label}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </body>
     </html>
--- components/WorkGrid.tsx
+++ components/WorkGrid.tsx
@@ -1,66 +1,86 @@
-import work from '../work'
+import work from "../work";
 
 interface Props {
-    id: string
+  id: string;
 }
 
-export default function WorkGrid({id}: Props) {
-    // @ts-ignore
-    const projects = work.reduce((acc, item) => {
-        if (!item.projects || (id !== "" && item.id !== id)) return acc;
-        return [
-            ...acc,
-            ...item.projects.map(proj => ({ ...proj, parent: item }))
-        ]
-    }, [])
+export default function WorkGrid({ id }: Props) {
+  // @ts-ignore
+  const projects = work.reduce((acc, item) => {
+    if (!item.projects || (id !== "" && item.id !== id)) return acc;
+    return [
+      ...acc,
+      ...item.projects.map((proj) => ({ ...proj, parent: item })),
+    ];
+  }, []);
 
-    return (
-        <div className="px-20 max-sm:px-10">
-            <nav>
-                <ul className="flex flex-row gap-5">
-                    <li>
-                        <a
-                            className="text-2xl font-light text-[Heebo] underline underline-offset-4 decoration-1 data-[focused=true]:font-semibold"
-                            href="/work"
-                            data-focused={id === ""}
-                        >
-                            All
-                        </a>
-                    </li>
-                    {work.map((section) => (
-                        <li key={section.id}>
-                            <a
-                                className="text-2xl font-light text-[Heebo] underline underline-offset-4 decoration-1 data-[focused=true]:font-semibold"
-                                data-focused={section.id === id}
-                                href={`/work/${section.id}`}
-                            >
-                                {section.name}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            <main className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 justify-center">
-                {
-                    //@ts-ignore
-                    projects.map((p) => (
-                        <a href={`/work/${p.parent.id}/${p.id}`} key={p.id}>
-                            <div className="flex flex-col mb-10 cursor-pointer">
-                                {p.hero && <img src={p.hero} className="max-w-max" />}
-                                <div className="h-44 flex flex-col">
-                                    <span className="mt-4 text-base text-[#2e2e2e] opacity-70 uppercase font-medium leading-tight">{p.parent.name}</span>
-                                    <span className="mt-2 font-[Heebo] font-normal text-2xl uppercase leading-tight">{p.name}</span>
-                                    <span className="mt-2 text-2xl text-[#2e2e2e] font-normal leading-tight">{p.description}</span>
-                                </div>
-                            </div>
-                        </a>
-                    ))
-                }
-            </main>
-            <div className="fixed bottom-0 left-0 w-screen bg-accent flex flex-row">
-                <a className="flex-1 text-2xl py-4 font-thin font-[Heebo] underline underline-offset-2 text-center" rel="noreferrer" href="mailto:andrearamosjn@gmail.com" target="_blank">Email Me</a>
-                <a className="flex-1 text-2xl py-4 font-thin font-[Heebo] underline underline-offset-2 text-center" rel="noreferrer" href="https://www.linkedin.com/in/andrearmsjn" target="_blank">LinkedIn</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="px-20 max-sm:px-5">
+      <nav>
+        <ul className="flex flex-row flex-wrap gap-x-5">
+          <li>
+            <a
+              className="text-2xl font-light text-[Heebo] data-[focused=true]:underline underline-offset-4 decoration-1 data-[focused=true]:font-semibold"
+              href="/work"
+              data-focused={id === ""}
+            >
+              All
+            </a>
+          </li>
+          {work.map((section) => (
+            <li key={section.id}>
+              <a
+                className="text-2xl font-light text-[Heebo] data-[focused=true]:underline underline-offset-4 decoration-1 data-[focused=true]:font-semibold"
+                data-focused={section.id === id}
+                href={`/work/${section.id}`}
+              >
+                {section.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 justify-center">
+        {
+          //@ts-ignore
+          projects.map((p) => (
+            <a href={`/work/${p.parent.id}/${p.id}`} key={p.id}>
+              <div className="flex flex-col mb-10 cursor-pointer">
+                {p.hero && <img src={p.hero} className="max-w-full" />}
+                <div className="h-44 flex flex-col">
+                  <span className="mt-4 text-base text-[#2e2e2e] opacity-70 uppercase font-medium leading-tight">
+                    {p.parent.name}
+                  </span>
+                  <span className="mt-2 font-[Heebo] font-normal text-2xl uppercase leading-tight">
+                    {p.name}
+                  </span>
+                  <span className="mt-2 text-2xl text-[#2e2e2e] font-normal leading-tight">
+                    {p.description}
+                  </span>
+                </div>
+              </div>
+            </a>
+          ))
+        }
+      </main>
+      <div className="fixed bottom-0 left-0 w-screen bg-accent flex flex-row">
+        <a
+          className="flex-1 text-2xl py-4 font-thin font-[Heebo] underline underline-offset-4 decoration-1 text-center"
+          rel="noreferrer"
+          href="mailto:andrearamosjn@gmail.com"
+          target="_blank"
+        >
+          Email Me
+        </a>
+        <a
+          className="flex-1 text-2xl py-4 font-thin font-[Heebo] underline underline-offset-4 decoration-1 text-center"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/andrearmsjn"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
 }
